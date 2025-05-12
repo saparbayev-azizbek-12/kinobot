bd9200533a34f107a99a5e12d87fff6cb604d6fe
Azizbek Saparbayev <saparbayev.azizbek12@gmail.com>
2025-05-12 16:08:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ env.read_env()
 
 API_TOKEN = env.str("API_TOKEN")
 print(env.str("ADMIN_ID"))
-ADMIN_ID = env.int("ADMIN_ID")
+ADMIN_ID = env.list("ADMIN_ID")
 MEDIA_CHANNEL_ID = env.int("MEDIA_CHANNEL_ID")
